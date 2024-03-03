44c395d19b84483c278a4c819b7eb0eb4a881ae7
Coderuff919 <Tuscany898@yahoo.com>
2024-03-02 16:24:37 -0800

    add content

--- dist/index.js
+++ dist/index.js
@@ -3,4 +3,11 @@ console.log('Hello Hunter');
 let age = 20;
 if (age < 50)
     age += 10;
+console.log(age);
+let sales = 123456789;
+let course = 'TypeScript';
+let is_published = true;
+let level;
+let numbers = [];
+numbers.forEach(n => n.toFixed);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,26 @@ console.log('Hello Hunter');
 let age: number = 20
 if (age < 50)
   age += 10;
+console.log(age);
+
+// let sales: number = 123_456_789;
+// let course: string = 'TypeScript';
+// let is_published: boolean = true;
+
+// You can remove the type annotation so it will be like below
+
+let sales = 123_456_789;
+let course = 'TypeScript';
+let is_published = true;
+//  any type
+let level; 
+
+// arrays
+// n. will give you option to choose for code completion
+let numbers: number[] = [];
+numbers.forEach(n => n.toFixed);
+
+// tuples
+
+
+
